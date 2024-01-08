5a9dfd364f18ab605dd53cef9d60c77f0f57b738
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-08 19:17:13 +0100

    deelte: import AppCard

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
 
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import AppCard from './components/AppCard.vue'
 
 export default {
   components: {
     AppHeader,
     AppMain,
-    AppCard
   }
 }
 
