9e528922a99fdb160ee6a7a92a461b193f82e06f
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-08 19:18:11 +0100

    include: AppCard componet

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,45 @@
 <script>
 
+import AppCard from './AppCard.vue'
+
 export default {
-    name: "AppMain"
+    name: "AppMain",
+
+    components: {
+        AppCard
+    }
 }
 
 </script>
 
-<template></template>
+<template>
+    <main>
+        <div class="main_container">
+            <AppCard />
+        </div>
+    </main>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+    background-color: bisque;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* debug */
+    // height: 500px;
+}
+
+.main_container {
+    width: 90%;
+    margin-top: 50px;
+    background-color: white;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
+    padding: 10px;
+    /*  dedbug */
+    // height: 200px;
+}
+</style>
