de42a10734af772a403e523fee9b54c56d2daf73
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-08 18:58:11 +0100

    add: content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,17 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    padding-left: 20px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+}
+</style>
