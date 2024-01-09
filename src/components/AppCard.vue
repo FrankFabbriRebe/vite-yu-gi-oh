bc422f8e4ce2ed876a063d9b7bdb685838b06d88
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-09 08:27:10 +0100

    amend: AppCard contrent

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -7,13 +7,21 @@ export default {
 </script>
 
 <template>
-    <div class="card"></div>
+    <div class="card">
+        <h1>Img</h1>
+        <h2>Name</h2>
+        <h3>Type</h3>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 .card {
     background-color: bisque;
     width: calc(100% / 5 - 10px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    // debug
     height: 100px;
 }
 </style>
