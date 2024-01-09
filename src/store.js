ed5cf9fcc2aaab19406d3115230207e469e7aba6
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-01-09 16:54:45 +0100

    fix: type

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ export const store = reactive({
     cardList: [],
     apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
     // array from where selct the type
-    archetype: [],
+    apiArchetyperchetype: [],
     apiArchetype: "https://db.ygoprodeck.com/api/v7/archetypes.php",
 
 })
